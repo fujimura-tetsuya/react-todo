188ba91a2b636c26ce196c2b1e86773c89d683ee
fujimura <38678061+fujimura-tetsuya@users.noreply.github.com>
2022-05-30 16:30:57 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,37 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <div>
+          <ul>
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+
+        <p>完了のTODO</p>
+        <div>
+          <ul>
+            <li>ううう</li>
+            <button>もどす</button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
